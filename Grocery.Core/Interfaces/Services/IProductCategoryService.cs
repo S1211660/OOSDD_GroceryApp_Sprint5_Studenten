2725b0baea7affd01b4580bbe3b5765bcc3acdcb
S1211660 <s1211660@student.windesheim.nl>
2025-10-09 10:47:52 +0200

    feat: ProductCategoryService toegevoegd
    Interface en service voor ProductCategoryService toegevoegd

---
+++ Grocery.Core/Interfaces/Services/IProductCategoryService.cs
@@ -0,0 +1,10 @@
+﻿using Grocery.Core.Models;
+
+namespace Grocery.Core.Interfaces.Services
+{
+    public interface IProductCategoryService
+    {
+        public List<Product> GetProductsByCategory(int categoryId);
+        public List<ProductCategory> GetAll();
+    }
+}
---
+++ Grocery.Core/Services/ProductCategoryService.cs
@@ -0,0 +1,44 @@
+﻿using Grocery.Core.Interfaces.Repositories;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+
+namespace Grocery.Core.Services
+{
+    public class ProductCategoryService : IProductCategoryService
+    {
+        private readonly IProductCategoryRepository _productCategoryRepository;
+        private readonly IProductRepository _productRepository;
+
+        public ProductCategoryService(
+            IProductCategoryRepository productCategoryRepository,
+            IProductRepository productRepository)
+        {
+            _productCategoryRepository = productCategoryRepository;
+            _productRepository = productRepository;
+        }
+
+        public List<Product> GetProductsByCategory(int categoryId)
+        {
+            var productCategories = _productCategoryRepository.GetAll()
+                .Where(pc => pc.CategoryId == categoryId)
+                .ToList();
+
+            var products = new List<Product>();
+            foreach (var pc in productCategories)
+            {
+                var product = _productRepository.Get(pc.ProductId);
+                if (product != null)
+                {
+                    products.Add(product);
+                }
+            }
+
+            return products;
+        }
+
+        public List<ProductCategory> GetAll()
+        {
+            return _productCategoryRepository.GetAll();
+        }
+    }
+}
