c31568673dfa20e46ba99a90933875843125fcc6
S1211660 <s1211660@student.windesheim.nl>
2025-10-09 11:09:58 +0200

    feat: ProductCategoriesView toegevoegd
    View en viewmodel toegevoegd voor ProductCategories

---
+++ Grocery.App/ViewModels/ProductCategoriesViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+
+namespace Grocery.App.ViewModels
+{
+    [QueryProperty(nameof(CategoryId), "categoryId")]
+    [QueryProperty(nameof(CategoryName), "categoryName")]
+    public partial class ProductCategoriesViewModel : BaseViewModel
+    {
+        private readonly IProductCategoryService _productCategoryService;
+        private ObservableCollection<Product> _products = new();
+        private string _categoryName = string.Empty;
+        private int _categoryId;
+
+        public int CategoryId
+        {
+            get => _categoryId;
+            set
+            {
+                _categoryId = value;
+                OnPropertyChanged();
+                LoadProducts();
+            }
+        }
+
+        public string CategoryName
+        {
+            get => _categoryName;
+            set
+            {
+                _categoryName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Product> Products
+        {
+            get => _products;
+            set
+            {
+                _products = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ProductCategoriesViewModel(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        private void LoadProducts()
+        {
+            var products = _productCategoryService.GetProductsByCategory(CategoryId);
+            Products = new ObservableCollection<Product>(products);
+        }
+    }
+}
